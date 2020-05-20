20e142254de09efdd32210a178fa1944102e37d4
jucnn <juliettecnn@gmail.com>
2020-05-20 12:11:03 +0200

    show all sessions and one session

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Sessions routes
+Route::get('/sessions', 'SessionController@index')->name('sessions.index');
+Route::get('/sessions/{id}', 'SessionController@show')->name('sessions.show');
+
